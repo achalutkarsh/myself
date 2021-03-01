e5e492711a4fde2d041df666a383156655e21d90
Achal Utkarsh <nissan@achal.local>
2021-03-01 22:48:12 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfessionalModule } from './professional/professional.module';
+import { SharedModule } from './shared/shared.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProfessionalModule,
+    SharedModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
